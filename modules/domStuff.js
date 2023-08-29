7bd98b4e8ac541228abb31d6ee3605caf30a39c8
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2023-08-29 15:54:46 +0530

    fix background alignment , change text decoration

--- modules/domStuff.js
+++ modules/domStuff.js
@@ -1,22 +1,25 @@
 export function domStuff(weatherInfo) {
-  const temp = document.getElementById("temp");
-  const image = document.querySelector("#image img");
-  const status = document.getElementById("status");
+    const temp = document.getElementById("temp");
+    const image = document.querySelector("#image img");
+    const status = document.getElementById("status");
 
-  temp.innerHTML = weatherInfo.temp + "<sup>°c</sup>";
-  status.innerText = weatherInfo.status;
-  image.src = weatherInfo.image;
+    temp.innerHTML = weatherInfo.temp + "<sup>°c</sup>";
+    status.innerText = weatherInfo.status;
+    image.src = weatherInfo.image;
 
-  setWallpaper(weatherInfo.status);
+    setWallpaper(weatherInfo.status);
 }
 
 async function setWallpaper(query) {
-  const body = document.querySelector("#wallpaper");
-  let wallpaper = await fetch(
-    `https://api.unsplash.com/search/photos?query=sky ${query}&client_id=KXB55fXOYigKnhXTI2ooOc9OJwxamNWipvNC80L8-nw&per_page=1`,
-    { mode: "cors" }
-  );
-  wallpaper = await wallpaper.json();
-  console.log(wallpaper);
-  body.src = wallpaper.results[0].urls.regular;
+    const body = document.querySelector("#wallpaper");
+    let wallpaper = await fetch(
+        `https://api.unsplash.com/search/photos?query=sky ${query}&client_id=KXB55fXOYigKnhXTI2ooOc9OJwxamNWipvNC80L8-nw&per_page=10`,
+        { mode: "cors" }
+    );
+    wallpaper = await wallpaper.json();
+    body.src = wallpaper.results[randomG()].urls.regular;
+}
+
+function randomG() {
+    return Math.floor(Math.random() * 10);
 }
--- modules/weather.js
+++ modules/weather.js
@@ -1,8 +1,8 @@
 export default class weather {
-  constructor(image, temp, dayOrNight, status) {
-    this.image = image;
-    this.temp = parseInt(temp);
-    this.dayOrNight = dayOrNight;
-    this.status = status;
-  }
+    constructor(image, temp, dayOrNight, status) {
+        this.image = image;
+        this.temp = parseInt(temp);
+        this.dayOrNight = dayOrNight;
+        this.status = status;
+    }
 }
